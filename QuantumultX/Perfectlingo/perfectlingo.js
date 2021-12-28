c78d29e684b98a640dde09f6a3e3b066e8238ac1
bin64 <yanmou1996@gmail.com>
2021-12-28 11:01:06 +0800

    Update perfectlingo.js

--- QuantumultX/Perfectlingo/perfectlingo.js
+++ QuantumultX/Perfectlingo/perfectlingo.js
@@ -5,6 +5,8 @@ let obj = JSON.parse(respbody);
 //message = obj.data.force_update;
 
 obj.data.force_update = false;
+
+obj.data.show_frequency = '1';
 // $notify('title', 'subTitle', message);
 
 // console.log('d');
