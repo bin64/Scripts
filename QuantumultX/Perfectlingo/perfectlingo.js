1cbf185320c246b847d1a193388acba5a9189a21
bin64 <yanmou1996@gmail.com>
2021-12-28 10:52:33 +0800

    Update perfectlingo.js

--- QuantumultX/Perfectlingo/perfectlingo.js
+++ QuantumultX/Perfectlingo/perfectlingo.js
@@ -1,10 +1,12 @@
-var respbody = response.body;
+var respbody = $response.body;
+
 let obj = JSON.parse(respbody);
 
 //message = obj.data.force_update;
 
 obj.data.force_update = false;
 // $notify('title', 'subTitle', message);
-// console.log(respbody);
+
+console.log('d');
 
 $done({body: JSON.stringify(obj)});
