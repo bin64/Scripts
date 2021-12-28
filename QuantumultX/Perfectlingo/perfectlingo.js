9a5a87627d11594dd451ef1659817e9d069d672c
bin64 <yanmou1996@gmail.com>
2021-12-28 10:25:22 +0800

    Create perfectlingo.js

---
+++ QuantumultX/Perfectlingo/perfectlingo.js
@@ -0,0 +1,10 @@
+var respbody = $response.body;
+let obj = JSON.parse(respbody);
+
+//message = obj.data.force_update;
+
+obj.data.force_update = 'false'
+// $notify('title', 'subTitle', message);
+console.log(respbody)
+
+$done({body: JSON.stringify(obj)});
