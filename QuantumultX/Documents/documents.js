042f32e80693d24d5c34439f3fadde252914680c
bin64 <yanmou1996@gmail.com>
2022-03-15 00:16:43 +0800

    Create documents.js

---
+++ QuantumultX/Documents/documents.js
@@ -0,0 +1,6 @@
+// var respbody = $response.body;
+
+
+
+// console.log(respbody)
+$done({body: JSON.stringify({originalTransactionId:550001043815434,inAppStates:[{originalTransactionId:550001043815434,productId:"com.readdle.ReaddleDocsIPad.subscription.month10_allusers",subscriptionState:"active",productName:"subscription",subscriptionExpirationDate:"15:28 21/03/2022",isEligibleForIntroPeriod:false,subscriptionAutoRenewStatus:"autoRenewOff",type:"subscription",isInGracePeriod:false,isInBillingRetryPeriod:false,entitlements:[]}],chargingPlatform:"iOS AppStore",receiptStatus:"ok",bundleId:"com.readdle.ReaddleDocsIPad",receiptId:1647270594000,statisticsInfo:{events:[]}})});
